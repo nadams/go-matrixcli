94954c74e9ef4bbaf814c141bdd7ea84e1f53499
Nick Adams <nadams@node-3.net>
2020-02-19 08:02:28 -0700

    Login if the account token is not found

--- auth/auth.go
+++ auth/auth.go
@@ -2,12 +2,14 @@ package auth
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
 	"time"
 
+	"github.com/matrix-org/gomatrix"
 	"github.com/nadams/go-matrixcli/config"
 )
 
@@ -64,5 +66,58 @@ func (t *TokenStore) Token(name string) (AccountAuth, error) {
 		}
 	}
 
-	return AccountAuth{}, nil
+	var found bool
+	var account config.Account
+
+	for _, a := range t.config.Accounts {
+		if a.Name == name {
+			found = true
+			account = a
+
+			break
+		}
+	}
+
+	if !found {
+		return AccountAuth{}, fmt.Errorf("could not found account in config: %s", name)
+	}
+
+	cl, err := gomatrix.NewClient(account.Homeserver, "", "")
+	if err != nil {
+		return AccountAuth{}, err
+	}
+
+	resp, err := cl.Login(&gomatrix.ReqLogin{
+		Type:     "m.login.password",
+		User:     account.Username,
+		Password: account.Password,
+	})
+
+	if err != nil {
+		return AccountAuth{}, err
+	}
+
+	aa := AccountAuth{
+		Name:   name,
+		UserID: resp.UserID,
+		Token:  resp.AccessToken,
+	}
+
+	t.accounts = append(t.accounts, aa)
+
+	f, err := os.Create(filepath.Join(t.config.CacheDir, "accounts.json"))
+	if err != nil {
+		return AccountAuth{}, nil
+	}
+
+	defer f.Close()
+
+	enc := json.NewEncoder(f)
+	enc.SetIndent("", "  ")
+
+	if err := enc.Encode(t.accounts); err != nil {
+		return AccountAuth{}, err
+	}
+
+	return aa, nil
 }
