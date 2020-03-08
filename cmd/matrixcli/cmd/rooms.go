ef1443a8da79e7fc1673addb5a5db57b83260374
Nick Adams <nadams@node-3.net>
2020-03-08 13:54:37 -0600

    List room members

--- cmd/matrixcli/cmd/rooms.go
+++ cmd/matrixcli/cmd/rooms.go
@@ -11,11 +11,12 @@ import (
 )
 
 type Rooms struct {
-	List ListRooms `cmd:"" help:"List joined rooms."`
+	List    ListRooms   `cmd:"" help:"List joined rooms."`
+	Members ListMembers `cmd:"" help:"List members in a given room."`
 }
 
 type ListRooms struct {
-	Account string `optional:"" short:"a" help:"Which account to send from."`
+	Account string `optional:"" short:"a" help:"Account to use."`
 }
 
 func (l *ListRooms) Run(ts *auth.TokenStore) error {
@@ -31,6 +32,7 @@ func (l *ListRooms) Run(ts *auth.TokenStore) error {
 	if err != nil {
 		return err
 	}
+
 	cl, err := gomatrix.NewClient(aa.Homeserver, aa.UserID, aa.Token)
 	if err != nil {
 		return err
@@ -49,6 +51,67 @@ func (l *ListRooms) Run(ts *auth.TokenStore) error {
 
 	for i, r := range rooms.JoinedRooms {
 		t.AppendRow(table.Row{i + 1, r})
+	}
+
+	t.Render()
+
+	return nil
+}
+
+type ListMembers struct {
+	Room    string `arg:"" help:"The room to list the members of."`
+	Account string `optional:"" short:"a" help:"Account to use."`
+}
+
+func (l *ListMembers) Run(ts *auth.TokenStore) error {
+	var aa auth.AccountAuth
+	var err error
+
+	if l.Account == "" {
+		aa, err = ts.Current()
+	} else {
+		aa, err = ts.Find(l.Account)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	cl, err := gomatrix.NewClient(aa.Homeserver, aa.UserID, aa.Token)
+	if err != nil {
+		return err
+	}
+
+	resp, err := cl.JoinedMembers(l.Room)
+	if err != nil {
+		return err
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Name", "User ID"})
+
+	type membership struct {
+		id   string
+		name string
+	}
+
+	members := make([]membership, 0, len(resp.Joined))
+	for id, user := range resp.Joined {
+		var name string
+		if n := user.DisplayName; n != nil {
+			name = *n
+		}
+
+		members = append(members, membership{id: id, name: name})
+	}
+
+	sort.Slice(members, func(i, j int) bool {
+		return members[i].name < members[j].name && members[i].id < members[j].id
+	})
+
+	for _, m := range members {
+		t.AppendRow(table.Row{m.name, m.id})
 	}
 
 	t.Render()
